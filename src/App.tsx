ed44a7323d79f84be3b345681294c462487af0c4
heppaat <pali.herrer@gmail.com>
2024-11-03 19:36:06 +0100

    feat(css): finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { toDo } from "./type";
-import { Button } from "@radix-ui/themes";
-
-import "./App.css";
+import { Button, Text } from "@radix-ui/themes";
 import CheckboxComp from "./Components/CheckboxComp";
 
 function App() {
@@ -42,37 +40,46 @@ function App() {
 
   console.log(toDo);
   return (
-    <>
-      <div>
-        <h1>To Do App</h1>
+    <div className="bg-gradient-to-r from-gray-900 to-purple-900 min-h-screen">
+      <div className="flex justify-center">
+        <h1 className="text-8xl font-black uppercase text-[#F9F6EE] my-20">
+          To Do App
+        </h1>
       </div>
-      <div>
+      <div className="flex justify-center items-center space-x-10">
         <input
-          className="border-4"
+          className="rounded-md border-2 border-gray-300 px-4 py-2 focus:ring focus:ring-teal-500 focus:outline-none"
           type="text"
-          placeholder="What to do"
+          placeholder="What to do..."
           value={input}
           onChange={(e) => setInput(e.target.value.trimStart())}
         />
-        <Button size="3" variant="surface" onClick={handleAddToDoList}>
-          Add
+        <Button size="3" variant="solid" onClick={handleAddToDoList}>
+          <Text size="2" weight="bold" className="text-[#1d1d1c]">
+            ADD
+          </Text>
         </Button>
       </div>
       {!error && (
-        <div>
+        <div className="flex flex-col items-center mt-20 space-y-4 pl-32">
           {toDo.map((listItem, index) => (
-            <div key={index} className="flex">
+            <div
+              key={index}
+              className="flex items-center space-x-8 w-full max-w-md"
+            >
               <CheckboxComp
                 checked={listItem.isComplete}
                 onChange={() => handleComplete(index)}
               />
-              <p>{listItem.text}</p>
+              <p className="text-[#F9F6EE] uppercase font-bold ">
+                {listItem.text}
+              </p>
             </div>
           ))}
         </div>
       )}
       {error && <p>{error}</p>}
-    </>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Theme accentColor="indigo">
+    <Theme accentColor="mint">
       <App />
     </Theme>
   </StrictMode>
